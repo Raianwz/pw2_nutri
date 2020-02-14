d28bab6e0c4a318f2e4171969b46a76fd5d9d447
raianwz <raian03@live.com>
2020-02-13 22:21:20 -0300

    Criação de Função e aperfeiçoamento do Código

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
-//pegar peso
-let paciente = document.querySelector(".paciente");
-let tdPeso = paciente.querySelector(".info-peso");
-let peso = tdPeso.textContent;
+let pacientes = document.querySelectorAll(".paciente");
 
-//pegar a altura
-let tdAltura = paciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
+pacientes.forEach(paciente => {
 
-let pesoValido = true;
-let alturaValida = true;
+//function cacularImc(paciente){
 
+        /* [Código Legado] - usado antes de criar a função criar calcularImc após a extinta função anonima
+            for (var i = 0; i < pacientes.length; i++) {
+            let paciente = pacientes[i]*/
 
-//validar dados
-if (peso <= 0 || peso >= 1000) {
-    console.error("peso inválido");
-    pesoValido = false;
-}
-if (altura <= 0) {
-    console.error("altura inválida");
-    alturaValida = false;
-}
+    //pegar peso 
+    let peso = paciente.querySelector(".info-peso").textContent;
+    //pegar a altura
+    let altura = paciente.querySelector(".info-altura").textContent;
 
+    let pesoValido = true;
+    let alturaValida = true;
 
-if (pesoValido && alturaValida) {
-    //calcular o IMC
-    let imc = peso / altura ** 2;
-
-    //escrever o IMC na Tabela
     let tdIMC = paciente.querySelector(".info-imc");
-    tdIMC.textContent = imc;
-}
+    //validar dados
+    if (peso <= 0 || peso >= 1000) {
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0) {
+        alturaValida = false;
+        tdIMC.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (pesoValido && alturaValida) {
+    //calcular o IMC
+        let imc = peso / altura ** 2;
+    //escrever o IMC na Tabela
+        tdIMC.textContent = imc.toFixed(2);
+    }
+});
